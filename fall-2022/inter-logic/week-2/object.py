03ad1211d1bd2d18a90308111c9765268d428ebd
jjeon02 <june.jeon@gmail.com>
2022-09-21 19:58:34 -0400

    update

--- fall-2022/inter-logic/week-2/object.py
+++ fall-2022/inter-logic/week-2/object.py
@@ -30,7 +30,7 @@ adjective = input("Enter an adjective: ")
 number = input("Any number you have in mind ")
 time = input("What time do you go to sleep? ")
 
-story = "I went to " + place + "to do a little exercise. The " + color + "sky was " + adjective + " today. While running, the " + animal + " suddenly popped up and frightened me. What in the " + place + "? I thought to myself. The " + animal + " spoke: It's been " + number + " years since we last met, why haven't you called me since? Then I woke up at " + time
+story = "I went to " + place + " to do a little exercise. The " + color + " sky was " + adjective + " today. While running, the " + animal + " suddenly popped up and frightened me. What in the " + place + "? I thought to myself. The " + animal + " spoke: It's been " + number + " years since we last met, why haven't you called me since? Then I woke up at " + time
 # finally we print the story
 print(story)
 
