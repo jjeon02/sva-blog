e3bc7321f939d2d33b857ee1263d72c256e2a188
jjeon02 <june.jeon@gmail.com>
2022-10-10 14:40:10 -0400

    hsdjfksal

--- fall-2022/inter-logic/loop/loop.js
+++ fall-2022/inter-logic/loop/loop.js
@@ -10,9 +10,12 @@ function draw() {
   background(0);
   noFill()
 
-  ellipse(mouseX, mouseY, 50, 50)
-  stroke(255,255,255)
+  // ellipse(mouseX, mouseY, 50, 50)
+  // stroke(255,255,255)
   rectMove()
+
+  angleMode(DEGREES);
+  rectMode(CENTER);
 }
 
 function rectMove(){
@@ -20,19 +23,19 @@ function rectMove(){
   widthSplit = width / 2
   heightSplit = height / 2
   translate(widthSplit, heightSplit)
+
   for (var i = 0; i < 200; i++ ){ 
-    angleMode(DEGREES);
-    rectMode(CENTER);
     push()
     rotate(sin(frameCount + i*2)* 100)
 
     var r = map(sin(frameCount), -1, -1, 50, 255)
     // var g = map(cos(frameCount/2), -1, -1, 50, 255)
     // var b = map(sin(frameCount/4), -1, -1, 50, 255)
-    stroke(r,255,255);
+    stroke(255,55,155,50);
 
-    rect (mouseX - widthSplit,mouseY - heightSplit,600 - i*3,600 - i*3,200-i);
-    rect (mouseX - widthSplit,mouseY - heightSplit,600 + i*3,600 + i*3,200 +i);
+    rect (0,0,600 - i*3,600 - i*3);
+
+    // rect (500,500,600 + i*3,600 + i*3);
     pop()
   }
 }
