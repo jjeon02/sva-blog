a7e89e64060fc66e46b55102a64f610e8e81f28c
jjeon02 <june.jeon@gmail.com>
2023-10-20 15:18:49 -0400

    update

---
+++ shadow-data/index.js
@@ -0,0 +1,9 @@
+
+// // NAVIGATION
+function openNav() {
+    document.getElementById("sideNav").style.width= "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("sideNav").style.width = "0";
+  }
