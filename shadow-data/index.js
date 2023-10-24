f2ccca1eb98a6578792ed8425a211db38480c34a
jjeon02 <june.jeon@gmail.com>
2023-10-23 20:23:26 -0400

    update

--- shadow-data/index.js
+++ shadow-data/index.js
@@ -1,9 +1,43 @@
-
 // // NAVIGATION
 function openNav() {
-    document.getElementById("sideNav").style.width= "100%";
-  }
+  document.getElementById("sideNav").style.width= "100%";
+}
   
-  function closeNav() {
-    document.getElementById("sideNav").style.width = "0";
-  }
+function closeNav() {
+  document.getElementById("sideNav").style.width = "0";
+}
+
+//P5.JS
+var sketchWidth;
+var sketchHeight;
+function setup() {
+  background(200, 200, 200);
+  sketchWidth = document.getElementById("canvas").offsetWidth;
+  sketchHeight = document.getElementById("canvas").offsetHeight;
+  
+  let renderer = createCanvas(sketchWidth, sketchHeight);
+  renderer.parent("canvas");
+}
+
+function draw() { 
+  mouseBrush();
+} 
+
+function mouseBrush(){
+  let mousetext = mouseX + '.' + mouseY;
+  const w = color(255, 255, 255);
+  const b = color(0,0,0)
+  fill(w);
+  stroke(b);
+  strokeWeight(2);  
+  textSize(30);
+  text(mousetext, mouseX, mouseY); 
+}
+
+function clearArt(){
+  clear(); 
+}
+
+function saveArt(){
+  save('art.jpg');
+}
